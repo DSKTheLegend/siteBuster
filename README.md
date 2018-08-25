a559be29060c2615e09e284055254466a19d3695
TheLegend <dsklegend@live.com>
2018-08-25 15:52:37 +0530

    Collective Output

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This is a light weight tool written in python which uses dictionary attack to check the available files on a website.
 It has a customizable range of extensions which you can change at your own wish.
 
+    Usage : siteBuster.py -u <siteurl> -w <wordlist> [[-p <port>][-s <response code>][-v]]
+
 ## Under Development!
 
 Feel free to send a PR and contribute to the code. Your name will be added to the author's list.
@@ -14,12 +16,13 @@ Feel free to send a PR and contribute to the code. Your name will be added to th
 
 
 ### ToDo
-        ##Buster Mode
-            Directory Buster Mode
-            File Buster Mode
-            Both
-        ## Verbose - Added on 25-Aug-2018
-        ## Other Valid Status Codes [200,204,301,302,307] - Added on 25-Aug-2018
-        ## Final List of Files / Directory Separately
-        ## False Positive   - Added on 25-Aug-2018
+        You tell me @ - dsklegend@live.com
+
+### ChangeLog
+        ## Verbose 
+        ## Other Valid Status Codes [200,204,301,302,307] 
+        ## Final List of Files / Directory Separately   
+        ## False Positive   
+        
+        
 ## TheLegend 
--- siteBuster.py
+++ siteBuster.py
@@ -5,9 +5,8 @@ import requesocks
 
 def siteBurst(url,wordlist,status_code,verbose):
     
-    fhandle = open(wordlist,'r')
-    words =  fhandle.read().split()
-    dir_found = list()
+    
+    words = wordlist.split()
     # Extentions of most of the web languages included here 
         ## ASP    ## CSS    ## Coldfusion    ## Erlang    ## Flash
         ## HTML   ## Java   ## Javascript    ## Perl      ## PHP
@@ -16,20 +15,27 @@ def siteBurst(url,wordlist,status_code,verbose):
     #extention = ['','.asp','.aspx','.axd','.asx','.asmx','.ashx','.cfm','.css','.yaws','.swf','.html','.htm','.xhtml','.jhtml','.jsp','.jspx','.wss','.do','.action','.js','.pl','.php','.php4','.php3','.phtml','.py','.rb','.rhtml','.shtml','.xml','.rss','.svg','.cgi','.dll']
     #Keeping only commonly found extentions 
     extention = ['','.asp','.aspx','.css','.html','.htm','.jsp','.jspx','.js','.php','.php4','.php3','.xml']    
+    print("We have "+str(len(words)*len(extention))+" combinations to try!")
     false_check = False # a flag to check if we are gettinng false positives or not 
+    directory = list()
+    pages = list()
     for word in words:
         false_positive = 0   # a flag to count the number of extentions work for a single word phrase
         for ext in extention :
-            req = requesocks.get(url+word+ext) # format -> [http://example.com][index][.html]
+            try:
+                req = requesocks.get(url+word+ext) # format -> [http://example.com][index][.html]
+            except:
+                print("Enountered an ERROR! Try again.")    
             code = req.status_code
             if str(code) in status_code.split(','): # if the site sends a positive response code
                 if ext == '':   #directory file
                     false_positive = false_positive + 1
-                    print("[+]"+url+word+ext + " directory found !")
-                    print('')
+                    print("[#]  "+url+word+ext + " directory found !")
+                    directory.append(url+word+ext)
                 else :  #non-directory file
                     false_positive = false_positive + 1
-                    print("[+]"+url+word+ext + " found !")
+                    print("[+]  "+url+word+ext + " found !")
+                    pages.append(url+word+ext)
                 if false_check is False:        # False Positive Check
                     if false_positive > 4:
                         print("We may be getting false positive results!.")
@@ -44,11 +50,29 @@ def siteBurst(url,wordlist,status_code,verbose):
                             quit()
             else :   # if the site sends a negative response code
                 if bool(verbose) == True:
-                    print("=# "+url+word+ext + " not found !")
+                    print("[*] "+url+word+ext + " not found !")
                 else:    
                     continue
 
-
+    print("\n\n")                
+    # Print the webpages found
+    print("==="+"="*max([len(page) for page in pages])+"===")
+    print("WebPages Found :")
+    print("==="+"="*max([len(page) for page in pages])+"===")
+    for page in pages:
+        print("[+] "+page)
+    print("==="+"="*max([len(page) for page in pages])+"===")
+    print('')
+    
+    #Print the directories found
+    print("==="+"="*max([len(folder) for folder in directory])+"===")
+    print("Directories Found :")
+    print("==="+"="*max([len(folder) for folder in directory])+"===")
+    for folder in directory:
+        print("[#] "+folder)
+    print("==="+"="*max([len(folder) for folder in directory])+"===")
+    
+    
 def checker(url,port,wordlist,status_code,verbose):
     #Checking is Site is alive or not
     mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -68,15 +92,15 @@ def checker(url,port,wordlist,status_code,verbose):
         
         fhandle = open(wordlist,'r')
         print("Wordlist : "+wordlist+" Found!")
-        print("We have "+str(len(fhandle.read().split())*len(status_code))+" combinations to try!")
+        
     except:
         print ("The file you provided : " + wordlist + " is not valid. Please check the path or filename and try again")  
         quit(0)
         
-    siteBurst(url,wordlist,status_code,verbose)  
+    siteBurst(url,fhandle.read(),status_code,verbose)  
         
 def main():
-    parser = optparse.OptionParser('usage: siteBuster.py ' + '-u <siteurl> -w <wordlist> [[-p <port>][-s <response code>][-v]')
+    parser = optparse.OptionParser('usage: siteBuster.py ' + '-u <siteurl> -w <wordlist> [[-p <port>][-s <response code>][-v]]')
     parser.add_option('-u', dest='url',type='string',help='enter the url of the site you want to burst')
     parser.add_option('-p', dest='port',type='string',help='enter the port(if site is running on any port other than 80)')
     parser.add_option('-w', dest='wordlist',type='string',help='enter the path of the wordlist you want to use')
